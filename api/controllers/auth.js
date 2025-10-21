805ac2a6257d7eef4444d589fa13164e9cf01918
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-10-21 10:44:37 +0530

    Refactor authentication controller to use async/await and improve error handling; update bcrypt import and usage. Simplify CORS middleware setup in index.js.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,68 +1,98 @@
 import { pool } from "../db.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const register = (req, res) => {
-    //CHECK USER IF EXISTS
+const register = async (req, res) => {
 
-    const q = "SELECT * FROM users WHERE username = ?";
+    try {
+        const { username, email, password, name } = req.body;
 
-    pool.query(q, [req.body.username], (err, data) => {
-        if (err) return res.status(500).json(err);
-        if (data.length) return res.status(409).json("User already exists!");
-        //CREATE A NEW USER
-        //Hash the password
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-
-        const q =
-            "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
-
-        const values = [
-            req.body.username,
-            req.body.email,
-            hashedPassword,
-            req.body.name,
-        ];
-
-        pool.query(q, [values], (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("User has been created.");
-        });
-    });
-};
-
-export const login = (req, res) => {
-    const q = "SELECT * FROM users WHERE username = ?";
-
-    pool.query(q, [req.body.username], (err, data) => {
-        if (err) return res.status(500).json(err);
-        if (data.length === 0) return res.status(404).json("User not found!");
-
-        const checkPassword = bcrypt.compareSync(
-            req.body.password,
-            data[0].password
-        );
-
-        if (!checkPassword)
-            return res.status(400).json("Wrong password or username!");
-
-        const token = jwt.sign({ id: data[0].id }, "secretkey");
-
-        const { password, ...others } = data[0];
-
-        res
-            .cookie("accessToken", token, {
-                httpOnly: true,
+        if (!username || !email || !password || !name) {
+            return res.status(400).json({
+                message: "All the fields are required."
             })
-            .status(200)
-            .json(others);
-    });
-};
+        }
 
-export const logout = (req, res) => {
+        const oldUser = await pool.query(`SELECT * FROM users WHERE username = ($1)`, [username])
+
+        if (oldUser) {
+            return res.status(409).json({
+                message: "User already exists!!"
+            })
+        }
+
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const newUser = await pool.query(`INSERT INTO users (username, password, email, name) VALUES ($1, $2, $3, $4) RETURNING username`, [username, password, email, name]);
+
+        res.status(201).json({
+            message: "user created successfully",
+            username: newUser.rows[0].username
+        })
+    } catch (error) {
+        //checking for any duplicate users
+        if (error.code === "23505") {
+            return res.status().json({
+                message: "Username already exists."
+            })
+        }
+
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        if (!username || !password) {
+            res.status(400).json({
+                message: "Username or password both are required"
+            })
+        }
+
+        const user = await pool.query(`SELECT * FROM users WHERE username = $1`, [username])
+
+        if (user.rows.length == 0) {
+            return res.status().json({
+                message: "Invalid username or password"
+            })
+        }
+
+        const isValid = await bcrypt.compare(password, user.rows[0].password)
+
+        if (!isValid) {
+            return res.status(401).json({
+                message: "Invalid username or password"
+            })
+        }
+
+        const token = jwt.sign({ id: user.data[0].id }, "secretkey");
+
+        res.cookie("accessToken", token, { httpOnly: true, })
+            .status(200)
+            .json({
+                id: user.rows[0].id,
+                username: user.rows[0].username,
+                name: user.rows[0].name
+            });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+const logout = (req, res) => {
     res.clearCookie("accessToken", {
         secure: true,
         sameSite: "none"
     }).status(200).json("User has been logged out.")
 };
+
+
+export default { register, login, logout };
